8551c91021b68a4065c89bd7b1540fcc3e4a7de7
vagrant <vagrant@homestead>
2019-03-22 13:05:41 +0000

    [TASK] - init commit

---
+++ src/LogServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Dimitricasierhowest\Log;
+
+use Illuminate\Support\ServiceProvider;
+
+class LogServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->loadRoutesFrom(__DIR__  . '/routes.php');
+    }
+}
---
+++ src/routes.php
@@ -0,0 +1,5 @@
+<?php
+
+\Route::get('/log-demo', function(){
+   return 'log demo!';
+});
